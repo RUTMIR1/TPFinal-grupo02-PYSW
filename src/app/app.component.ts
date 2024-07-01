2d878100544c13fcfc3e0a3b85fe139af8dfa518
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2024-07-01 15:50:58 -0300

    page login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AnunciosComponent } from './components/anuncios/anuncios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule,RouterOutlet, LoginComponent, AnunciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AnunciosComponent } from './components/anuncios/anuncios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'anuncios', component: AnunciosComponent },
+    { path: '**', redirectTo: 'anuncios', pathMatch: 'full' }
+];
