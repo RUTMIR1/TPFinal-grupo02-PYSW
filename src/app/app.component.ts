31e05512995da7280988a1cc2a4f01d31e36fb8c
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2024-07-03 16:02:10 -0300

    agregando footer y header de front-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AnunciosComponent } from './components/anuncios/anuncios.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule,RouterOutlet, LoginComponent, AnunciosComponent],
+  imports: [RouterModule,RouterOutlet,FooterComponent,HeaderComponent, LoginComponent, AnunciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
