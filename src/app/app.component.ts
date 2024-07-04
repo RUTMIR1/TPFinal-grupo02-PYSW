f16889908b8b2dab9d669df8e3e4d8a06891e72c
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-07-04 13:26:35 -0300

    borrador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,14 @@ import { LoginComponent } from './components/login/login.component';
 import { NovedadesComponent } from './components/novedades/novedades.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { CrudUsuariosComponent } from './components/crud-usuarios/crud-usuarios.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule,RouterOutlet,CrudUsuariosComponent, LoginComponent, NovedadesComponent,RegistroComponent],
+  imports: [RouterModule,RouterOutlet,CrudUsuariosComponent, LoginComponent, NovedadesComponent,RegistroComponent,HomeComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
