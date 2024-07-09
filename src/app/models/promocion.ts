fde8d4d9efdaa5ecd6fee43564a72467494c61a4
Marusero-ju <65613970+Marusero-ju@users.noreply.github.com>
2024-07-09 14:38:35 -0300

    Add files via upload

---
+++ src/app/models/promocion.ts
@@ -0,0 +1,13 @@
+export class Promocion {
+
+    _id!: string;
+    pathing!: string;
+    descripcion!: string;
+
+    constructor(_id: string, pathing: string, descripcion: string){
+        this._id = _id;
+        this.pathing= pathing;
+        this.descripcion = descripcion;
+
+    }
+}
