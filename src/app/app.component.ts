b7b085250b592a0f0c16c4fda6b6edc4c672cb15
Marusero-ju <65613970+Marusero-ju@users.noreply.github.com>
2024-07-11 15:06:14 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,14 @@ import { AlquilerComponent } from './components/alquiler/alquiler.component';
 import { LocalesFormComponent } from './components/locales-form/locales-form.component';
 import { LocalesListComponent } from './components/locales-list/locales-list.component';
 import { CrudNovedadesComponent } from './components/crud-novedades/crud-novedades.component';
+import { CrudAnunciosComponent } from './components/crud-anuncios/crud-anuncios.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule,RouterOutlet,CrudUsuariosComponent, LoginComponent, NovedadesComponent,RegistroComponent,HomeComponent,HeaderComponent,FooterComponent,
-            LocalesFormComponent, LocalesListComponent,AlquilerComponent,HttpClientModule, CrudNovedadesComponent
+            LocalesFormComponent, LocalesListComponent,AlquilerComponent,HttpClientModule, CrudNovedadesComponent,CrudAnunciosComponent
            ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
