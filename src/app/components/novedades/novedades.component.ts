991c49d1290e34a99c8191b4a1cfd91bf433f0e6
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-07-11 15:16:03 -0300

    vista de novedades

--- src/app/components/novedades/novedades.component.ts
+++ src/app/components/novedades/novedades.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Novedad } from '../../models/novedad';
+import { Router } from '@angular/router';
+import { NovedadService } from '../../services/novedad.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-novedades',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './novedades.component.html',
   styleUrl: './novedades.component.css'
 })
 export class NovedadesComponent {
+  arrayNovedades!: Array<Novedad>;
+
+  constructor( private novedadesService: NovedadService, private router:Router){ 
+    this.obtenerNovedades();
+  }
+
+  //  OBTENER LAS NOVEDADES DE LA BASE DE DATOS
+  obtenerNovedades() {
+    this.novedadesService.getNovedades().subscribe(
+      data => {
+        this.arrayNovedades = data;
+        console.log(this.arrayNovedades);
+    },
+    error =>{
+      console.log(error)
+    }
+  )
+  }
 
 }
