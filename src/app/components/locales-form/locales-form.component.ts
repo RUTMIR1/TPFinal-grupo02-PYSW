615ad1c68388f3c0b4398dca87420ed9f7ea31bc
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-07-11 00:50:20 -0300

    Crud Locales terminado, assets de locales, locales.txt

--- src/app/components/locales-form/locales-form.component.ts
+++ src/app/components/locales-form/locales-form.component.ts
@@ -1,102 +1,190 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { Local } from '../../models/local';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LocalesListComponent } from '../locales-list/locales-list.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LocalService } from '../../services/local.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { SafeCall } from '@angular/compiler';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-locales-form',
   standalone: true,
-  imports: [FormsModule,CommonModule,LocalesListComponent],
+  imports: [FormsModule, CommonModule, LocalesListComponent],
   templateUrl: './locales-form.component.html',
   styleUrl: './locales-form.component.css'
 })
-export class LocalesFormComponent {
+export class LocalesFormComponent implements OnInit {
 
   Local!: Local;
 
   accion: string = "new";
 
-  constructor(private activatedRoute: ActivatedRoute, private router:Router, private localService:LocalService ){
+  files: { base64: string, safeurl: SafeUrl }[] = [];
+  imagen64!: string;
+  //files!: { base64: string, safeurl: SafeUrl }[] = [];
+  toastrSvc = inject(ToastrService);
+
+  @ViewChild('selectHabilitado') selectHabilitadoRef!: ElementRef;
+
+  ngAfterViewInit() {
+    // Acceder al valor seleccionado del select
+    //console.log(this.selectHabilitadoRef.nativeElement.value);
+  }
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private localService: LocalService, private domSanitizer: DomSanitizer) {
     this.iniciarVariable();
   }
 
 
   // NG ONINIT 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
-      if (params['id'] == undefined){
-        
-        
-        this.accion = "new";
-      }else{
-        this.accion = "update";
-        console.log(params['id']);
-        this.cargarLocal(params['id']);
-      }
-  })};
+    let usuarioSession = sessionStorage.getItem("perfil") || "nadie";
+
+    if (usuarioSession != "nadie") {
+      let safeurl: SafeUrl = this.domSanitizer;
+      //this.files.push({ 'base64': "", 'safeurl': safeurl });
+      this.activatedRoute.params.subscribe(params => {
+        if (params['id'] == undefined) {
+          this.accion = "new";
+        } else {
+          this.accion = "update";
+          this.cargarLocal(params['id']);
+        }
+      })
+    } else {
+      this.router.navigate(['home']);
+    }
+
+  };
 
 
   // INSTANCIAR EL OBJETO LOCAL
-  iniciarVariable():void {
+  iniciarVariable(): void {
     this.Local = new Local();
   }
 
 
   //CREAR LOCAL
-  registrar():void {
-    this.localService.add(this.Local).subscribe(
-      (result) => {
-        console.log(result);
-        
-        if (result.status == 1) {
-          alert("Local Cargado")
-          this.router.navigate(['locales-list']);
-        }
-      },
-      error =>{
-        console.log(error)
+  registrar(): void {
+    //console.log(this.Local);
+    try {
+      this.Local.pathimages = this.files[0].base64 || "";
+      this.Local.alquilado = false;
+      this.Local.promociones.push();
+    } catch (error) {
+      console.log("ESTOY ACÁ ERROR");
     }
-    );
-    this.Local = new Local();
+    //
+    if (this.Local.nombre && this.Local.superficie && this.Local.habilitado && this.Local.alquilado !== null && this.Local.costoMes && this.Local.pathimages) {
+      try {
+        this.localService.add(this.Local).subscribe(
+          (result) => {
+            if (result.status == 1) {
+              this.toastrSvc.success('Local guardado con exito!!!', 'LOCAL GUARDADO', {
+                timeOut: 6000,
+              });
+              this.router.navigate(['locales-list']);
+            }
+          },
+          // ERROR AL GUARDAR EL LOCAL
+          error => {
+            this.toastrSvc.error('No se pudo guardar el local SORY :(', 'ERROR AL GUARDAR LOCAL', {
+              timeOut: 6000,
+            });
+            this.router.navigate(['locales-list']);
+          }
+        );
+      } catch (error) {
+        //ERROR AL GUARDAR EL LOCAL
+        this.toastrSvc.error('No se pudo guardar el local', 'ERROR AL GUARDAR LOCAL', {
+          timeOut: 6000,
+        });
+        this.router.navigate(['locales-list']);
+      }
+      this.Local = new Local();
+    } else {
+      //LLENAR TODOS LOS CAMPOS
+      this.toastrSvc.warning('Por favor llene todos los campos del formulario >:|', 'ERROR ENVIO DE FORMULARIO', {
+        timeOut: 6000,
+      });
+      this.router.navigate(['locales-form']);
+    }
+
   }
 
 
   // CARGA DE LOCAL
-  cargarLocal(id:string):void{
+  cargarLocal(id: string): void {
     this.localService.getLocalById(id).subscribe(
       (result) => {
-       Object.assign(this.Local,result);
-      }, 
-      error =>{
+        Object.assign(this.Local, result);
+        this.toastrSvc.info("El local " + this.Local.nombre + " está listo para editar", "Local Encontrado", {
+          timeOut: 5000
+        });
+      },
+      error => {
         console.log(error)
-    }
+        this.toastrSvc.error("El local " + this.Local.nombre + " no se encontró", "ERROR", {
+          timeOut: 4000
+        });
+      }
     );
-    this.Local = new Local();
+    //this.Local = new Local();
   }
 
   //UPDATE DE LOCAL
-  actualizar(){
+  actualizar() {
+    if (this.files.length != 0) {
+      this.Local.pathimages = this.files[0].base64;
+    }
     this.localService.update(this.Local).subscribe(
       (result) => {
-        console.log(result);
-        
         if (result.status == 1) {
-          alert("Local Actualizado")
           this.router.navigate(['locales-list']);
+          this.toastrSvc.success("El local " + this.Local.nombre + " ha sido actualizado con éxito", "LOCAL ACTUALIZADO", {
+            timeOut: 5000
+          });
         }
       },
-      error =>{
+      error => {
         console.log(error)
-    }
+        this.toastrSvc.error("El local " + this.Local.nombre + " no se pudo actualizar", "ERROR", {
+          timeOut: 4000
+        });
+      }
     );
     this.Local = new Local();
   }
 
+  onFileSelected(event: any) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      //inicio lector de archivo
+      const reader = new FileReader();
+      //declaro el comportamiento del onload cuando el reader carga o lee algo
+      reader.onload = () => {
+        let base64 = reader.result as string;
+        let safeurl: SafeUrl = this.domSanitizer.bypassSecurityTrustUrl(base64);
+        this.files.push({ 'base64': base64, 'safeurl': safeurl });
+      };
+      //hago que el reader lea un archivo
+      reader.readAsDataURL(file);
+    }
+  }
+  //PONER EL VALOR DEL HABILITADO
+  onChangeHabilitado() {
+    this.Local.habilitado = this.selectHabilitadoRef.nativeElement.value
+  }
 
-
+  //BTN CANCELAR
+  cancelar() {
+    this.Local = new Local;
+    this.router.navigate(['locales-list']);
+  }
 
 
 
--- src/app/components/locales-list/locales-list.component.ts
+++ src/app/components/locales-list/locales-list.component.ts
@@ -1,21 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Local } from '../../models/local';
 import { LocalService } from '../../services/local.service';
 import { Router } from '@angular/router';
+import { DataTablesModule } from "angular-datatables";
+import { Config } from 'datatables.net';
+import { config, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-locales-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DataTablesModule],
   templateUrl: './locales-list.component.html',
   styleUrl: './locales-list.component.css'
 })
-export class LocalesListComponent {
+export class LocalesListComponent implements OnInit, AfterViewInit {
 
+  dtOptions: any = {};
+  dtTrigger: Subject<any> = new Subject<any>();
   arrayLocales!: Array<Local>;
 
-  constructor( private localService: LocalService, private router:Router){ 
+  constructor(private localService: LocalService, private router: Router) {
     this.obtenerLocales();
   }
 
@@ -26,45 +31,79 @@ export class LocalesListComponent {
       data => {
         this.arrayLocales = data;
         console.log(this.arrayLocales);
-    },
-    error =>{
-      console.log(error)
+        this.dtTrigger.next(0);
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+  ngOnInit() {
+    let usuarioSession = sessionStorage.getItem("perfil") || "nadie";
+    if (usuarioSession != "nadie") {
+      this.arrayLocales = new Array<Local>();
+      this.obtenerLocales();
+      // this.dtOptions = {
+      //   language: {
+      //     url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+      //   },
+      //   responsive: true
+      // }
+    } else {
+      this.router.navigate(['home']);
     }
-  )
+
+
+  }
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
+
   // ADD => ME ENVIA AL FORMULARIO PARA DAR DE ALTA LOCALES
-  agregar(){
+  agregar() {
     this.router.navigate(['locales-form']);
   }
 
- // UPDATE => ME ENVIA AL FORMULARIO CON LOS DATOS CARGADOS DEL LOCAL PARA SU MODIFICACIÓN
-  modificar(id: string){
+  // UPDATE => ME ENVIA AL FORMULARIO CON LOS DATOS CARGADOS DEL LOCAL PARA SU MODIFICACIÓN
+  modificar(id: string) {
     this.router.navigate(['locales-form', id]);
   }
 
 
   // DELETE, ELIMINA EL LOCAL
-  eliminar(id:string){
+  eliminar(id: string) {
     this.localService.deleteLocal(id).subscribe(
-      
+
       (result) => {
         console.log(result);
-        
+
         if (result.status == 1) {
           alert("Producto eliminado Correctamente")
           this.obtenerLocales();
         }
       },
-      error =>{
+      error => {
         console.log(error)
-    }
+      }
     );
   }
 
 
+  ngAfterViewInit(): void {
+    this.dtOptions = {
+      language: {
+        url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+      },
+      responsive: true
+    };
+
+    // Inicializa DataTables
+    // $(document).ready(() => {
+    //   $('#example').DataTable(this.dtOptions);
+    // });
+  }
 
 
-  
 
 }
