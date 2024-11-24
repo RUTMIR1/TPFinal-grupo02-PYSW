ca5fe8b88a97c600f563057ffa9c0228b15ed6ea
Rutmir1 <theaxeblue@gmail.com>
2024-11-24 00:20:07 -0300

    nuevos cambios

--- src/app/models/usuario.spec.ts
+++ src/app/models/usuario.spec.ts
@@ -4,4 +4,10 @@ describe('Usuario', () => {
   it('should create an instance', () => {
     expect(new Usuario()).toBeTruthy();
   });
+
+  it('siempre falla', () => {
+    const usuario = new Usuario('Juan', 'Pérez', 'juan.perez@example.com', 'juan123', 'password', 123456789, 'admin');
+    // Una expectativa incorrecta para que la prueba falle siempre
+    expect(usuario.nombre).toBe('Incorrecto');
+  });
 });
